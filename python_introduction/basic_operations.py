160fc4f9b55df60543d2b45688ec7bdbe8016b32
Sunday Onoja <sundayonoja23@gmail.com>
2025-09-13 18:08:02 +0100

    Added basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+# Assign values to two variables
+number1 = 10
+number2 = 5
+
+# Arithmetic operations
+addition = number1 + number2 is 15
+subtraction = number1 - number2 is 5
+multiplication = number1 * number2 is 50

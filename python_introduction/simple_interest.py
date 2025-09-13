a7e5c284d5c3cbb3e3648923f41c30d2214129f5
Sunday Onoja <sundayonoja23@gmail.com>
2025-09-13 18:40:57 +0100

    added simple interest script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# Simple Interest Calculator
+
+# Defining variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#Formula
+interest = principal * rate * time
+
+# Result
+print(f"The simple interest is: {interest}")

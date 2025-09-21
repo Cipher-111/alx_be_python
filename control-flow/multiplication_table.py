3cf433b540cbf964b42ed0921b95845d5f2b67b3
Sunday Onoja <sundayonoja23@gmail.com>
2025-09-21 21:27:25 +0100

    added mutiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,10 @@
+#Multiplication Table Generator
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and print the multiplication table
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
